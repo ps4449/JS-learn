1b0e08941fbea45c3d722a084a9db731de0d2861
Priyanka Srinivas <priyasrini55@gmail.com>
2023-06-27 23:39:38 +0530

    Basic files upload

--- qr-code-generator/script.js
+++ qr-code-generator/script.js
@@ -1,3 +1,4 @@
-function ABC() {
-    
+function generateQR() {
+    gAPI = "https://chart.googleapis.com/chart?chs=250x100&cht=qr&chl=Hello|World";
+    var text = document.getElementById("qrtext").value;
 }
